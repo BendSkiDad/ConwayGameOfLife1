7605ec381b6bbaa8325a4c4f985fa37d2a5fee0e
Gene Hyde <Gene@HydeEnterprises.com>
2024-02-21 10:05:21 -0800

    generate html with elements instead of sttrings, convert for loop to filter funcction

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 "use strict";
 
-//todo: generate html dynamically instead of with strings
 //todo: store the live cells in the GameOfLifeLogic module and access them there instead of generating the live cells from the Html. This will require adding or removing a cell upon click of the TD element
 
 var GridUtilities = function () {
@@ -46,21 +45,14 @@ var GameOfLifeLogic = function () {
     }
 
     function deriveNumberOfLiveNeighbors(rowIndex, columnIndex, liveCells) {
-        //todo: consider doing this with a filter function?
-        var count = 0;
-        for (var i = 0; i < liveCells.length; i++) {
-            var liveCell = liveCells[i];
-            if (
-                liveCell.rowIndex >= rowIndex - 1
+        var liveNeighborCells = liveCells.filter(function (liveCell) {
+            return liveCell.rowIndex >= rowIndex - 1
                 && liveCell.rowIndex <= rowIndex + 1
                 && liveCell.columnIndex >= columnIndex - 1
                 && liveCell.columnIndex <= columnIndex + 1
-                && !(liveCell.rowIndex === rowIndex && liveCell.columnIndex === columnIndex)
-            ) {
-                count++;
-            }
-        }
-        return count;
+                && !(liveCell.rowIndex === rowIndex && liveCell.columnIndex === columnIndex);
+        });
+        return liveNeighborCells.length;
     }
 
     function deriveNewLiveCellsFrom(liveCells) {
@@ -92,70 +84,91 @@ var GameOfLifeLogic = function () {
 }();
 
 var GameOfLifeHtmlGeneration_HtmlTable = function () {
+    const rowIndexAttributeName = "rowIndex";
+    const columnIndexAttributeName = "columnIndex";
+    const iterationCountElementId = "iterationCount";
+    const liveAttributeName = "live";
+    const liveCellCSSClassToken = "liveCell";
+    const gameOfLifeCellCSSClassToken = "cell";
+
     function getRowIndexFrom(cellTDElement) {
-        var rowIndex = Number(cellTDElement.getAttribute("rowIndex"));
+        var rowIndex = Number(cellTDElement.getAttribute(rowIndexAttributeName));
         return rowIndex;
     }
 
     function getColumnIndexFrom(cellTDElement) {
-        var columnIndex = Number(cellTDElement.getAttribute("columnIndex"));
+        var columnIndex = Number(cellTDElement.getAttribute(columnIndexAttributeName));
         return columnIndex;
     }
 
     function updateIterationCount() {
-        var iterationCountDiv = document.getElementById("iterationCount");
+        var iterationCountDiv = document.getElementById(iterationCountElementId);
         var iterationCount = Number(iterationCountDiv.textContent);
         iterationCountDiv.textContent = iterationCount + 1;
     }
 
-    function generateBoardHeaderHtmlRowFrom(leftColumnIndex, rightColumnIndex) {
-        var headerOutputRow = "<tr>" + "<th class='noPadding'>" + "&nbsp" + "</th>";
+    function generateBoardHeaderTRElementFrom(leftColumnIndex, rightColumnIndex) {
+        var firstTHElement = document.createElement("th");
+        firstTHElement.classList.add("noPadding");
+        firstTHElement.innerHTML = "&nbsp";
+        var rc = document.createElement("tr");
+        rc.appendChild(firstTHElement);
         for (var i = leftColumnIndex; i <= rightColumnIndex; i++) {
-            headerOutputRow += "<th class='noPadding'>" + Math.abs(i % 10) + "</th>";
+            var nextTHElement = document.createElement("th");
+            nextTHElement.classList.add("noPadding");
+            var textNode = document.createTextNode(Math.abs(i % 10));
+            nextTHElement.appendChild(textNode);
+            rc.appendChild(nextTHElement);
         }
-        return headerOutputRow + "</tr>";
+        return rc;
     }
 
-    function generateRowHtmlFrom(liveCells, rowIndex, leftColumnIndex, rightColumnIndex) {
-        var rowText = "<tr>" + "<td class='noPadding'>" + "<strong>" + Math.abs(rowIndex % 10) + "</strong>" + "</td>";
+    function generateRowElementFrom(liveCells, rowIndex, leftColumnIndex, rightColumnIndex) {
+        var tdElement = document.createElement("td");
+        tdElement.classList.add("noPadding");
+        var textNode = document.createTextNode(Math.abs(rowIndex % 10));
+        tdElement.appendChild(textNode);
+        var rc = document.createElement("tr");
+        rc.appendChild(tdElement);
         for (var columnIndex = leftColumnIndex; columnIndex <= rightColumnIndex; columnIndex++) {
-            var TDElement = document.createElement("td");
-            TDElement.setAttribute("rowIndex", rowIndex);
-            TDElement.setAttribute("columnIndex", columnIndex);
-            TDElement.classList.add("cell");
-            TDElement.setAttribute("onclick", "GameOfLifeEventHandlerModule.handleCellClick(this)");
+            var tdElement = document.createElement("td");
+            tdElement.setAttribute(rowIndexAttributeName, rowIndex);
+            tdElement.setAttribute(columnIndexAttributeName, columnIndex);
+            tdElement.classList.add(gameOfLifeCellCSSClassToken);
+            tdElement.setAttribute("onclick", "GameOfLifeEventHandlerModule.handleCellClick(this)");
             if (GameOfLifeLogic.isThereALiveCellAt(liveCells, rowIndex, columnIndex)) {
-                TDElement.setAttribute("live", "");
-                TDElement.classList.add("liveCell");
+                tdElement.setAttribute(liveAttributeName, "");
+                tdElement.classList.add(liveCellCSSClassToken);
             }
             else {
-                TDElement.removeAttribute("live");
-                TDElement.innerHTML = "&nbsp";
+                tdElement.removeAttribute(liveAttributeName);
+                tdElement.innerHTML = "&nbsp";
             }
-            rowText += TDElement.outerHTML;
+            rc.appendChild(tdElement);
         }
-        return rowText + "</tr>";
+        return rc;
     }
 
-    function generateBoardHtmlFrom(liveCells, boardCoordinates) {
-        var board = "<table>" + generateBoardHeaderHtmlRowFrom(boardCoordinates.minColumnIndex, boardCoordinates.maxColumnIndex);
+    function generateBoardAsTableHtmlElementFrom(liveCells, boardCoordinates) {
+        var tableElement = document.createElement("table");
+        tableElement.appendChild(generateBoardHeaderTRElementFrom(boardCoordinates.minColumnIndex, boardCoordinates.maxColumnIndex));
         for (var rowIndex = boardCoordinates.minRowIndex; rowIndex <= boardCoordinates.maxRowIndex; rowIndex++) {
-            board += generateRowHtmlFrom(liveCells, rowIndex, boardCoordinates.minColumnIndex, boardCoordinates.maxColumnIndex);
+            tableElement.appendChild(generateRowElementFrom(liveCells, rowIndex, boardCoordinates.minColumnIndex, boardCoordinates.maxColumnIndex));
         }
-        return board + "</table>";
+        return tableElement;
     }
 
-    function renderBoard(boardHtml) {
-        document.querySelector("#board").innerHTML = boardHtml;
+    function renderBoard(boardAsHtmlTableElement) {
+        document.querySelector("#board").replaceChildren(boardAsHtmlTableElement);
     }
 
     function deriveLiveCellsFromBoard() {
-        var cellTDElements = document.querySelectorAll(".cell");
+        var cellTDElements = document.querySelectorAll("." + gameOfLifeCellCSSClassToken);
         //todo: consider doing this with a map function?
         var liveCells = new Array();
         for (var i = 0; i < cellTDElements.length; i++) {
             var cellTDElement = cellTDElements[i];
-            var isLive = cellTDElement.hasAttribute("live");
+            var isLive = cellTDElement.hasAttribute(liveAttributeName);
             if (isLive) {
                 var rowIndex = getRowIndexFrom(cellTDElement);
                 var columnIndex = getColumnIndexFrom(cellTDElement);
@@ -166,7 +179,7 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
     }
 
     function deriveBoardOuterCoordinates() {
-        var cellTDElements = document.querySelectorAll(".cell");
+        var cellTDElements = document.querySelectorAll("." + gameOfLifeCellCSSClassToken);
         var rowIndexes = Array.from(cellTDElements).map(function (cellTDElement) {
             return getRowIndexFrom(cellTDElement);
         })
@@ -195,25 +208,29 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
         boardOuterCoordinates.minColumnIndex = Math.min(boardOuterCoordinates.minColumnIndex, outerNewLiveCellCoordinates.minColumnIndex);
         boardOuterCoordinates.maxRowIndex = Math.max(boardOuterCoordinates.maxRowIndex, outerNewLiveCellCoordinates.maxRowIndex);
         boardOuterCoordinates.maxColumnIndex = Math.max(boardOuterCoordinates.maxColumnIndex, outerNewLiveCellCoordinates.maxColumnIndex);
-        var boardHtml = generateBoardHtmlFrom(newLiveCells, boardOuterCoordinates);
-        renderBoard(boardHtml);
+        var boardAsHtmlTableElement = generateBoardAsTableHtmlElementFrom(newLiveCells, boardOuterCoordinates);
+        renderBoard(boardAsHtmlTableElement);
     }
 
     function reRenderBoardWithNew(boardOuterCoordinates) {
         var liveCells = deriveLiveCellsFromBoard();
-        var boardHtml = generateBoardHtmlFrom(liveCells, boardOuterCoordinates);
-        renderBoard(boardHtml);
+        var boardAsHtmlTableElement = generateBoardAsTableHtmlElementFrom(liveCells, boardOuterCoordinates);
+        renderBoard(boardAsHtmlTableElement);
     }
 
     function toggleLiveness(tdElement) {
-        if (tdElement.hasAttribute("live")) {
-            tdElement.removeAttribute("live");
-            tdElement.classList.remove("liveCell");
+        if (tdElement.hasAttribute(liveAttributeName)) {
+            tdElement.removeAttribute(liveAttributeName);
+            tdElement.classList.remove(liveCellCSSClassToken);
         }
         else {
-            tdElement.setAttribute("live", "");
-            tdElement.classList.add("liveCell");
+            tdElement.setAttribute(liveAttributeName, "");
+            tdElement.classList.add(liveCellCSSClassToken);
         }
+        return {
+            rowIndex: getRowIndexFrom(tdElement),
+            columnIndex: getColumnIndexFrom(tdElement)
+        };
     }
 
     function advanceAStep() {
@@ -231,7 +248,6 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
         var boardOuterCoordinates = deriveBoardOuterCoordinates();
         boardOuterCoordinates.maxColumnIndex += 1;
         reRenderBoardWithNew(boardOuterCoordinates);
-
     }
 
     function stop() {
@@ -246,7 +262,7 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
         if (isRunning) {
             stop();
         }
-        var iterationCountDiv = document.getElementById("iterationCount");
+        var iterationCountDiv = document.getElementById(iterationCountElementId);
         iterationCountDiv.textContent = 0;
         var liveCells = [
             //up and left traveler
@@ -270,8 +286,8 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
             maxRowIndex: 10,
             maxColumnIndex: 80
         };
-        var boardHtml = generateBoardHtmlFrom(liveCells, startingBoardCoordinates);
-        renderBoard(boardHtml);
+        var boardAsHtmlTableElement = generateBoardAsTableHtmlElementFrom(liveCells, startingBoardCoordinates);
+        renderBoard(boardAsHtmlTableElement);
     }
 
     function start() {
@@ -295,7 +311,7 @@ var GameOfLifeHtmlGeneration_HtmlTable = function () {
 
 var GameOfLifeEventHandlerModule = function (gameOfLifeHtmlGenerationModule) {
     function handleCellClick(tdElement) {
-        gameOfLifeHtmlGenerationModule.toggleLiveness(tdElement);
+        var elementCoordinates = gameOfLifeHtmlGenerationModule.toggleLiveness(tdElement);
     }
 
     function handleAdvanceAStepClick() {
@@ -334,6 +350,6 @@ var GameOfLifeEventHandlerModule = function (gameOfLifeHtmlGenerationModule) {
         handleRunClick: handleRunClick,
         handleStopClick: handleStopClick
     };
-}(GameOfLifeHtmlGeneration_HtmlTable);  //inject the HtmlTable version of GOL HtmlGeneration module. Currently this is the only one but I'm thinking of writing another.
+}(GameOfLifeHtmlGeneration_HtmlTable);  //inject the HtmlTable version of GOLHtmlGeneration module. Currently this is the only one but I'm thinking of writing another.
 
 GameOfLifeHtmlGeneration_HtmlTable.reset();
